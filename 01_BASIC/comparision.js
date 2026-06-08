6b065cd8561cdcc3efe78e2d5f2ab4b68f5d3944
Sakshi saini <sakshisaini3536@gmail.com>
2026-06-08 09:05:57 +0000

    Comparision

---
+++ 01_BASIC/comparision.js
@@ -0,0 +1,16 @@
+console.log(2 > 1);
+console.log(2 >= 1);
+console.log(2 < 1);
+console.log(2 == 1);
+console.log(2 != 1);
+console.log("02" > 1);
+console.log("2" > 1);
+// confusing and prefer to not use this type of conversion
+console.log(null > 0);
+console.log(null == 0);
+console.log(null >= 0);
+console.log(undefined == 0);
+console.log(undefined > 0);
+console.log(undefined < 0);
+// === strict check this not just perform conversion but also check data type
+console.log("2" === 2);
